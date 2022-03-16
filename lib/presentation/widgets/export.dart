24f370b90d2151602536ca422872a909e738d1d7
JasperEssien2 <jasperessien2@gmail.com>
2022-03-16 10:34:44 +0100

    added error and loading widget

--- lib/presentation/widgets/export.dart
+++ lib/presentation/widgets/export.dart
@@ -1,6 +1,7 @@
+export 'error_widget.dart';
 export 'google_button.dart';
 export 'item_artist.dart';
 export 'item_mate.dart';
 export 'item_select_artist.dart';
-export 'mates_connect.dart';
+export 'loading_spinner.dart';
 export 'mates_connect.dart';
